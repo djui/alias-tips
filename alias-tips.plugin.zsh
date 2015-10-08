822a32b056466825fa428df679bfe58f00eac92f
Uwe Dauernheim <uwe@dauernheim.net>
2015-10-08 10:45:13 +0100

    Hint at all supported feature variables
    Change-Id: I5335ce38e155c3a5d5a4c92f4c0216265d38f97f

--- alias-tips.plugin.zsh
+++ alias-tips.plugin.zsh
@@ -1,5 +1,9 @@
 PLUGIN_DIR=$(dirname $0)
 
+#export ZSH_PLUGINS_ALIAS_TIPS_TEXT="💡 Alias tip: "
+#export ZSH_PLUGINS_ALIAS_TIPS_EXCLUDES="_ c"
+#export ZSH_PLUGINS_ALIAS_TIPS_EXPAND=1
+
 preexec() {
   alias | ${PLUGIN_DIR}/alias-tips $*
 }
