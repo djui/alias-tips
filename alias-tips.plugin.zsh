58f23694694d91e409994f8ea6ee89c2958e4855
Tom Prince <tom.prince@twistedmatrix.com>
2015-11-03 16:19:34 -0700

    Use hook rather than preexec.
    This allows alias-tips to cooperate with other plugins that want to use `preexec`.

--- alias-tips.plugin.zsh
+++ alias-tips.plugin.zsh
@@ -4,6 +4,9 @@ PLUGIN_DIR=$(dirname $0)
 #export ZSH_PLUGINS_ALIAS_TIPS_EXCLUDES="_ c"
 #export ZSH_PLUGINS_ALIAS_TIPS_EXPAND=1
 
-preexec() {
+_alias-tips_preexec () {
   alias | ${PLUGIN_DIR}/alias-tips $*
 }
+
+autoload -Uz add-zsh-hook
+add-zsh-hook preexec _alias-tips_preexec
