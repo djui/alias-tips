e84d8b4a2eeb9d445c1b1c4e192301bc2a37db7d
Tom Prince <tom.prince@twistedmatrix.com>
2015-11-03 16:47:41 -0700

    Use consistent naming for internal things.

--- alias-tips.plugin.zsh
+++ alias-tips.plugin.zsh
@@ -4,9 +4,9 @@ _alias_tips__PLUGIN_DIR=$(dirname $0)
 #export ZSH_PLUGINS_ALIAS_TIPS_EXCLUDES="_ c"
 #export ZSH_PLUGINS_ALIAS_TIPS_EXPAND=1
 
-_alias-tips_preexec () {
+_alias_tips__preexec () {
   alias | ${_alias_tips__PLUGIN_DIR}/alias-tips $*
 }
 
 autoload -Uz add-zsh-hook
-add-zsh-hook preexec _alias-tips_preexec
+add-zsh-hook preexec _alias_tips__preexec
