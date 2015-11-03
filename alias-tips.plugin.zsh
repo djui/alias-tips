9323fab6a6d2231248821bb514b2eb652d8e9dd7
Tom Prince <tom.prince@twistedmatrix.com>
2015-11-03 16:29:22 -0700

    Don't globally set PLUGIN_DIR.

--- alias-tips.plugin.zsh
+++ alias-tips.plugin.zsh
@@ -1,11 +1,11 @@
-PLUGIN_DIR=$(dirname $0)
+_alias_tips__PLUGIN_DIR=$(dirname $0)
 
 #export ZSH_PLUGINS_ALIAS_TIPS_TEXT="💡 Alias tip: "
 #export ZSH_PLUGINS_ALIAS_TIPS_EXCLUDES="_ c"
 #export ZSH_PLUGINS_ALIAS_TIPS_EXPAND=1
 
 _alias-tips_preexec () {
-  alias | ${PLUGIN_DIR}/alias-tips $*
+  alias | ${_alias_tips__PLUGIN_DIR}/alias-tips $*
 }
 
 autoload -Uz add-zsh-hook
