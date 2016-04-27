9d43ab9053d75704302a8f430524a9ef04907532
Uwe Dauernheim <uwe@dauernheim.net>
2016-04-27 22:51:55 +0900

    Combine alias output from zsh-builtins and git
    This allows ZSH_PLUGINS_ALIAS_TIPS_EXPAND=1 to work for a hybrid of git and
    zsh-builtins aliases.

--- alias-tips.plugin.zsh
+++ alias-tips.plugin.zsh
@@ -6,15 +6,15 @@ _alias_tips__PLUGIN_DIR=$(dirname $0)
 
 _alias_tips__preexec () {
   if hash git 2> /dev/null; then
-    \git alias | \
+    git_aliases=$(\git alias | \
       sed 's/^/git /' | \
       sed 's/ = \([^!]\)/ = git \1/' | \
-      sed 's/ = !/ = /' | \
-      ${_alias_tips__PLUGIN_DIR}/alias-tips $* && \
-      return
+      sed 's/ = !/ = /')
   fi
 
-  alias | ${_alias_tips__PLUGIN_DIR}/alias-tips $*
+  shell_aliases=$(alias)
+
+  echo $git_aliases $shell_aliases | ${_alias_tips__PLUGIN_DIR}/alias-tips $*
 }
 
 autoload -Uz add-zsh-hook
