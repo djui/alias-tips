9fe873ed627abe34cf07d7092c3d4e3a188a1053
Uwe Dauernheim <uwe@dauernheim.net>
2016-04-27 22:30:53 +0900

    Check git aliases first if git exists
    Change-Id: I27386a7a62e86ac538fd6ac639447f40375923ef

--- alias-tips.plugin.zsh
+++ alias-tips.plugin.zsh
@@ -5,6 +5,15 @@ _alias_tips__PLUGIN_DIR=$(dirname $0)
 #export ZSH_PLUGINS_ALIAS_TIPS_EXPAND=1
 
 _alias_tips__preexec () {
+  if hash git 2> /dev/null; then
+    \git alias | \
+      sed 's/^/git /' | \
+      sed 's/ = \([^!]\)/ = git \1/' | \
+      sed 's/ = !/ = /' | \
+      ${_alias_tips__PLUGIN_DIR}/alias-tips $* && \
+      return
+  fi
+
   alias | ${_alias_tips__PLUGIN_DIR}/alias-tips $*
 }
 
