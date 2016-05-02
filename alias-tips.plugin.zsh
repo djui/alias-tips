89b1d7c92f7adfbde33dc44e62ee7c1d6552aa07
Uwe Dauernheim <uwe@dauernheim.net>
2016-05-03 00:31:00 +0900

    Use git-config instead of git-alias
    git-alias is an additional tool by git-extras.

--- alias-tips.plugin.zsh
+++ alias-tips.plugin.zsh
@@ -6,8 +6,12 @@ _alias_tips__PLUGIN_DIR=$(dirname $0)
 
 _alias_tips__preexec () {
   if hash git 2> /dev/null; then
-    git_aliases=$(\git alias | \
-      sed 's/^/git /' | \
+
+    # alias.foo bar      => git foo = git bar
+    # alias.foo !git bar => git foo = git bar
+    git_aliases=$(git config --get-regexp "^alias\." | \
+      sed 's/[ ]/ = /' | \
+      sed 's/^alias\./git /' | \
       sed 's/ = \([^!]\)/ = git \1/' | \
       sed 's/ = !/ = /')
   fi
