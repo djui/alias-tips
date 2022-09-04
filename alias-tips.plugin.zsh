c9a62ab292eb4b7c8c0afc13c5ee46539d1f62fc
burningserenity <burningserenity@novo-ordo.com>
2022-09-04 11:32:35 -0400

    change egrep to grep -E to silence obsolescence warning

--- alias-tips.plugin.zsh
+++ alias-tips.plugin.zsh
@@ -43,7 +43,7 @@ _alias_tips__preexec () {
   shell_aliases=$(\alias)
 
   local shell_functions
-  shell_functions=$(\functions | \egrep -a '^[a-zA-Z].+ \(\) \{$')
+  shell_functions=$(\functions | \grep -E -a '^[a-zA-Z].+ \(\) \{$')
 
   # Exit code returned from python script when we want to force use of aliases.
   local force_exit_code=10
